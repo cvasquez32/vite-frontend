df36602a910157cbbe9342116d53d234349d9135
carlos vasquez <cvasquez0100@gmail.com>
2025-05-21 14:37:24 -0500

    use the ipify api to retrieve user ip address

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import self from "./assets/self.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [ipAddress, setIpAddress] = useState("");
+
+  useEffect(() => {
+    const fetchIpAddress = async () => {
+      const apiUrl = import.meta.env.VITE_IPIFY_API_KEY;
+
+      try {
+        const response = await fetch(`${apiUrl}`);
+
+        if (!response.ok) {
+          throw new Error(`API error ${response.status}`);
+        }
+
+        const data = await response.json();
+        setIpAddress(data.ip);
+      } catch (err) {
+        setIpAddress("IP not available");
+        console.log(err.message);
+      }
+    };
+    fetchIpAddress();
+  }, []);
 
   return (
     <>
@@ -15,8 +38,15 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a
+          href="https://www.linkedin.com/in/carlos-vasquez-mke/"
+          target="_blank"
+        >
+          <img src={self} width={100} height={120} />
+        </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>${ipAddress}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +59,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
